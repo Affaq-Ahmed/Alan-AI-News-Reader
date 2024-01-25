9d50a8318309336650ac7249a5a310077207d077
Affaq-Ahmed <aaawan165@gmail.com>
2024-01-25 18:46:41 +0500

    Add NewsCard component and handle error in NewsCards component

---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,70 @@
+import {
+	Button,
+	Card,
+	CardActionArea,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Typography,
+} from "@mui/material";
+import React from "react";
+
+const NewsCard = ({ article, i }) => {
+	return (
+		<Card
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "space-between",
+				borderBottom: "10px solid white",
+			}}
+		>
+			<CardActionArea href={article.url} target="_blank">
+				<CardMedia image={article.urlToImage} style={{ height: "250px" }} />
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "space-between",
+						margin: "20px",
+					}}
+				>
+					<Typography variant="body2" color="textSecondary" component="h2">
+						{new Date(article.publishedAt).toDateString()}
+					</Typography>
+					<Typography variant="body2" color="textSecondary" component="h2">
+						{article.source.name}
+					</Typography>
+				</div>
+				<Typography
+					gutterBottom
+					variant="h5"
+					component="h2"
+					style={{ padding: "0 16px" }}
+				>
+					{article.title}
+				</Typography>
+				<CardContent>
+					<Typography variant="body2" color="textSecondary" component="p">
+						{article.description}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions
+				style={{
+					padding: "0px 16px 8px 16px",
+					display: "flex",
+					justifyContent: "space-between",
+				}}
+			>
+				<Button size="small" color="primary">
+					Learn More
+				</Button>
+				<Typography variant="body2" color="textSecondary" component="p">
+					{i + 1}
+				</Typography>
+			</CardActions>
+		</Card>
+	);
+};
+
+export default NewsCard;
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NewsCard from "../NewsCard/NewsCard";
-import { Grid, Grow } from "@mui/material";
+import { Grid, Grow, Typography } from "@mui/material";
 
 const NewsCards = ({ articles }) => {
 	return (
@@ -15,19 +15,25 @@ const NewsCards = ({ articles }) => {
 					margin: 0,
 				}}
 			>
-				{articles.map((article, index) => (
-					<Grid
-						item
-						xs={12}
-						sm={6}
-						md={4}
-						lg={3}
-						key={index}
-						style={{ display: "flex" }}
-					>
-						<NewsCard article={article} />
-					</Grid>
-				))}
+				{articles ? (
+					articles.map((article, index) => (
+						<Grid
+							item
+							xs={12}
+							sm={6}
+							md={4}
+							lg={3}
+							key={index}
+							style={{ display: "flex" }}
+						>
+							<NewsCard article={article} i={index} />
+						</Grid>
+					))
+				) : (
+					<Typography variant="h2" component="h2" color="textSecondary">
+						Something Went wrong while fetching the News.
+					</Typography>
+				)}
 			</Grid>
 		</Grow>
 	);
